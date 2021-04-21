d41a489f1ce784a926acbbfeaacedb899548b867
peterhobson <nawrot_marcin@outlook.com>
2021-04-21 23:28:05 +0200

    v5

--- src/pl/javastart/library/app/app/Library.java
+++ src/pl/javastart/library/app/app/Library.java
@@ -1,6 +1,9 @@
+package pl.javastart.library.app.app;
+import pl.javastart.library.app.model.*;
+
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296,
                 "Greg", "9788373271890");
